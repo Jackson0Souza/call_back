10adb3781aa1efdb92a3523a6daff69fabc445ad
Jackson0Souza <129568316+Jackson0Souza@users.noreply.github.com>
2024-07-09 19:18:43 +0000

    callBack

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ fs.readFile("./arq1.txt", function(error, value){
         return
     }
     console.log(`conteudo : ${value}`);
+    
+    fs.readFile("./arq2.txt", function(error, value){
+    if (error){
+        console.error('deu ruim', error.message);
+        return
+    }
+    console.log(`conteudo : ${value}`);
 
 })
+
+fs.readFile("./arq3.txt", function(error, value){
+    if (error){
+        console.error('deu ruim', error.message);
+        return
+    }
+    console.log(`conteudo : ${value}`);
+
+})
+
+})
+
+
+
+
+
+
 
